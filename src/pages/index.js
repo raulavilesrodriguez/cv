49478bc7f582b2852d17c435fbb6fa3ffaf800cd
raulavilesrodriguez <finbravilesr@gmail.com>
2023-06-30 17:00:49 -0500

    add spaces

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,8 +66,9 @@ const IndexPage = () => (
               <div className="subheading mb-3">Arcotel</div>
               <div>
               <div>
-              Data analysis through R to determine the impact the interconnection charges in telecommunications industry 
-              <br>.    </br>  
+              Data analysis through R to determine the impact the interconnection charges in 
+              telecommunications industry. 
+              <br>Example: </br>  
               <a href="https://github.com/raulavilesrodriguez/interconexion_r">interconexion repository</a>
               <br> </br>
               <a href="https://ideaviles.shinyapps.io/interconexionr/">interconexion web app</a>
