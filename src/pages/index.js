6fafee020114fd8685a34c477134b9584b3c5adf
raulavilesrodriguez <finbravilesr@gmail.com>
2023-06-30 17:06:06 -0500

    add spaces

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,8 +68,7 @@ const IndexPage = () => (
               <div>
               Data analysis through R to determine the impact the interconnection charges in 
               telecommunications industry 
-              <p>.  &nbsp; 
-              </p>  
+              <p>.  &nbsp;</p>  
               <a href="https://github.com/raulavilesrodriguez/interconexion_r">interconexion repository</a>
               <br> </br>
               <a href="https://ideaviles.shinyapps.io/interconexionr/">interconexion web app</a>
